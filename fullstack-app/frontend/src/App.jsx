2c6e08ccabaff3e9d9885444cdcb6090652748f8
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-01-20 20:33:59 +0700

    2-frontend UI done

--- fullstack-app/frontend/src/App.jsx
+++ fullstack-app/frontend/src/App.jsx
@@ -1,36 +1,37 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { format } from 'date-fns';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-
+import { Container, Stack, Text } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
+import UserGrid from './components/UserGrid';
+import { useState } from 'react';
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Fullstack App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Stack minH={'100vh'}>
+      <Navbar setUsers={setUsers} />
+
+      <Container maxW={'1200px'} my={4}>
+        <Text
+          fontSize={{ base: '3xl', md: '50' }}
+          fontWeight={'bold'}
+          letterSpacing={'2px'}
+          textTransform={'uppercase'}
+          textAlign={'center'}
+          mb={8}
+        >
+          <Text
+            as={'span'}
+            bgGradient={'linear(to-r, cyan.400, blue.500)'}
+            bgClip={'text'}
+          >
+            My Contacts
+          </Text>
+          🚀
+        </Text>
+
+        <UserGrid users={users} setUsers={setUsers} />
+      </Container>
+    </Stack>
   );
 }
 
